7d4d06b860b89f155294b2bf8107c367dd0717a7
Christian Spinelli <c.spinelli93@gmail.com>
2021-02-20 13:48:45 -0300

    correcoes de text

--- src/utils/validations/UserValidation.ts
+++ src/utils/validations/UserValidation.ts
@@ -3,26 +3,26 @@ import * as yup from 'yup';
 export const UserSchema: any = yup.object().shape({
 	cpf: yup
 		.string()
-		.min(11, 'CPF tem 11 caracteres')
-		.required('Por favor insira seu cpf'),
-	name: yup.string().required('Por favor insira seu nome'),
-	username: yup.string().required('Por favor insira nome de e-mail'),
+		.min(11, 'O CPF deve ter 11 caracteres.')
+		.required('Por favor, insira seu CPF.'),
+	name: yup.string().required('Por favor, insira seu nome.'),
+	username: yup.string().required('Por favor, insira seu nome de e-mail.'),
 	password: yup
 		.string()
-		.min(4, 'Senha precisa ter no minimo quatro caracteres')
-		.required('Por favor escolha uma senha')
+		.min(4, 'A senha precisa ter no mínimo quatro caracteres.')
+		.required('Por favor escolha uma senha.')
 		.matches(
 			/^.*(?=.{4,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-			'A senha precisa ter 4 letras, uma letra maisculá, um numero e um caracter especial '
+			'A senha precisa ter 4 letras, uma letra maiúscula, um número e um caractere especial. '
 		),
 	password2: yup
 		.string()
-		.required('Por favor confirme sua senha')
+		.required('Por favor, confirme sua senha.')
 		.when('password', {
 			is: (password: string) =>
 				password && password.length > 0 ? true : false,
 			then: yup
 				.string()
-				.oneOf([yup.ref('password')], 'Senhas informadas não são iguais'),
+				.oneOf([yup.ref('password')], 'As senhas informadas não são iguais.'),
 		}),
 });
